780ddccc3ca8cfe0d868f448b797b562f572ab25
Gabriel Silveira <gabriel.cage@gmail.com>
2024-08-05 20:15:09 -0300

    como criar parte do email

--- form/enviar.php
+++ form/enviar.php
@@ -3,7 +3,9 @@ $nome = $_POST['nome'];/*Conteúdo que chegar para mim via POST do campo 'nome'*
 $email = $_POST['email'];
 $mensagem = $_POST['mensagem'];
 
-echo $nome;
-echo $email;
-echo $mensagem;
+
+$conteudo = utf8_decode('Nome ' .$nome . "\r\n". "\r\n" . ' Email: ' .$email . "\r\n". "\r\n" . "\r\n" . 'Mensagem:' .$mensagem ."\r\n". "\r\n");
+
+
+echo $conteudo;/*Imprime o conteudo da varivavel $conteúdo*/
 ?>
