ef97d5ea31197b692cb3b3a480c027dffdf65ae7
Gabriel Silveira <gabriel.cage@gmail.com>
2024-08-07 21:31:07 -0300

    aula sobre email em php

--- form/enviar.php
+++ form/enviar.php
@@ -2,10 +2,17 @@
 $nome = $_POST['nome'];/*Conteúdo que chegar para mim via POST do campo 'nome'*/
 $email = $_POST['email'];
 $mensagem = $_POST['mensagem'];
+$assunto =  "Email do Site";
+$remetente =  'gabriel.cage@gmail.com'  ;    
 
+echo $nome;
+echo $email;
+echo $mensagem;
 
-$conteudo = utf8_decode('Nome ' .$nome . "\r\n". "\r\n" . ' Email: ' .$email . "\r\n". "\r\n" . "\r\n" . 'Mensagem:' .$mensagem ."\r\n". "\r\n");
+$conteudo = utf8_decode('Nome: ' .$nome ."\r\n"."\r\n"."\r\n". '
+Email:'.$email ."\r\n"."\r\n". 'Mensagem:' . $mensagem."\r\n"."\r\n");
 
+$cabeçalho = "From: " .$email;
 
-echo $conteudo;/*Imprime o conteudo da varivavel $conteúdo*/
+mail($remetente, $assunto,$mensagem, $cabecalho);
 ?>
